5a4987a6ad3dbc5f17c519e804213dcf4be6f94f
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-04-13 09:42:44 +1000

    codewars: better than average

--- app.ts
+++ app.ts
@@ -1,38 +1,8 @@
-function countSheep(arrayofSheep: (boolean | undefined | null)[]): number {
-  let count: number = 0;
-  arrayofSheep.forEach((value) => {
-    if (value) {
-      count++;
-    }
-  });
-  return count;
+function betterThanAverage(classPoints: number[], yourPoints:number):boolean{
+    let average:number= (classPoints.reduce((currTotal, currValue)=> currTotal + currValue))/classPoints.length
+    return yourPoints > average ? true : false
 }
 
-let array1 = [
-  true,
-  true,
-  true,
-  false,
-  true,
-  true,
-  true,
-  true,
-  true,
-  false,
-  true,
-  false,
-  true,
-  false,
-  false,
-  true,
-  true,
-  true,
-  true,
-  true,
-  false,
-  false,
-  true,
-  true,
-];
-
-console.log(countSheep(array1));
+console.log(betterThanAverage([2, 3], 5))
+console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75))
+console.log(betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50)
