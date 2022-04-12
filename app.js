f69b7760204b138ba4c88adb087181aa8bb054bc
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-04-12 11:29:09 +1000

    codewars: findAverage

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 "use strict";
-function cockroachSpeed(s) {
-    return Math.floor((s * 100000) / 3600);
+function findAverage(array) {
+    return (array.length !== 0) ? (array.reduce((currTotal, currValue) => currTotal + currValue)) / array.length : 0;
 }
-console.log(cockroachSpeed(1.08));
+console.log(findAverage([1, 1, 1]));
+console.log(findAverage([10, 20, 30]));
+console.log(findAverage([]));
