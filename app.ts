14980010ad301dfd165f60a08f551be4589dcae2
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-04-13 10:35:36 +1000

    codewars: getCount

--- app.ts
+++ app.ts
@@ -1,8 +1,7 @@
-function betterThanAverage(classPoints: number[], yourPoints:number):boolean{
-    let average:number= (classPoints.reduce((currTotal, currValue)=> currTotal + currValue))/classPoints.length
-    return yourPoints > average ? true : false
+function getCount(str: string): number {
+  const regex = /[aeiou]/g;
+  const found = str.match(regex);
+  return found ? found.length : 0;
 }
 
-console.log(betterThanAverage([2, 3], 5))
-console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75))
-console.log(betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50)
+console.log(getCount("abracadabra"));
