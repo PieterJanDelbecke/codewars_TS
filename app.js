0512d81ef662e01b2fa8fe5c17d761ab858ad3f9
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-04-14 10:19:41 +1000

    typescript: private and public access modifiers

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
+    }
+    describe() {
+        console.log('Department: ' + this.name);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
 const accounting = new Department('Accounting');
-console.log(accounting);
+accounting.addEmployee('Max');
+accounting.addEmployee('Pieter');
+accounting.printEmployeeInformation();
