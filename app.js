715f006e7e101b4071a5caa66b3cc2f72dc2a53f
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-04-18 10:43:31 +1000

    typescript: overriding properties & protected modifier

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Department = /** @class */ (function () {
-    function Department(id, name) {
+"use strict";
+class Department {
+    constructor(id, name) {
         this.id = id;
         this.name = name;
         // private readonly id: string;
@@ -23,45 +9,44 @@ var Department = /** @class */ (function () {
         // this.id = id;
         // this.name = n;
     }
-    Department.prototype.describe = function () {
-        console.log("Department (".concat(this.id, "): ").concat(this.name));
-    };
-    Department.prototype.addEmployee = function (employee) {
+    describe() {
+        console.log(`Department (${this.id}): ${this.name}`);
+    }
+    addEmployee(employee) {
         // validation etc
         // this.id = 'd2';
         this.employees.push(employee);
-    };
-    Department.prototype.printEmployeeInformation = function () {
+    }
+    printEmployeeInformation() {
         console.log(this.employees.length);
         console.log(this.employees);
-    };
-    return Department;
-}());
-var ITDepartment = /** @class */ (function (_super) {
-    __extends(ITDepartment, _super);
-    function ITDepartment(id, admins) {
-        var _this = _super.call(this, id, 'IT') || this;
-        _this.admins = admins;
-        return _this;
     }
-    return ITDepartment;
-}(Department));
-var AccountingDepartment = /** @class */ (function (_super) {
-    __extends(AccountingDepartment, _super);
-    function AccountingDepartment(id, reports) {
-        var _this = _super.call(this, id, 'Accounting') || this;
-        _this.reports = reports;
-        return _this;
+}
+class ITDepartment extends Department {
+    constructor(id, admins) {
+        super(id, 'IT');
+        this.admins = admins;
     }
-    AccountingDepartment.prototype.addReport = function (text) {
+}
+class AccountingDepartment extends Department {
+    constructor(id, reports) {
+        super(id, 'Accounting');
+        this.reports = reports;
+    }
+    addEmployee(name) {
+        if (name === 'Max') {
+            return;
+        }
+        this.employees.push(name);
+    }
+    addReport(text) {
         this.reports.push(text);
-    };
-    AccountingDepartment.prototype.printReports = function () {
+    }
+    printReports() {
         console.log(this.reports);
-    };
-    return AccountingDepartment;
-}(Department));
-var it = new ITDepartment('d1', ['Max']);
+    }
+}
+const it = new ITDepartment('d1', ['Max']);
 it.addEmployee('Max');
 it.addEmployee('Manu');
 // it.employees[2] = 'Anna';
@@ -69,7 +54,7 @@ it.describe();
 it.name = 'NEW NAME';
 it.printEmployeeInformation();
 console.log(it);
-var accounting = new AccountingDepartment('d2', []);
+const accounting = new AccountingDepartment('d2', []);
 accounting.addReport('Something went wrong...');
 accounting.printReports();
 // const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
