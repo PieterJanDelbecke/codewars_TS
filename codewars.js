c083869989e28342170da15a59084f907a6a9953
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-04-26 16:08:31 +1000

    codewars: getCount

--- codewars.js
+++ codewars.js
@@ -1,5 +1,8 @@
 "use strict";
-function descendingOrder(n) {
-    return +n.toString().split("").map(e => +e).sort((a, b) => b - a).join("");
+function getCount(str) {
+    let regex = /[aouie]/gi;
+    let arr = str.match(regex);
+    return arr ? arr.length : 0;
 }
-console.log(descendingOrder(123456789));
+console.log(getCount("abracadabra"));
+console.log(getCount("dcd"));
--- codewars.ts
+++ codewars.ts
@@ -1,5 +1,8 @@
-function descendingOrder(n:number){
-    return +n.toString().split("").map(e => +e).sort((a,b) => b-a).join("")
+function getCount(str:string){
+    let regex = /[aouie]/gi
+    let arr =  str.match(regex)
+    return arr ? arr.length : 0
 }
 
-console.log(descendingOrder(123456789))
+console.log(getCount("abracadabra"))
+console.log(getCount("dcd"))
