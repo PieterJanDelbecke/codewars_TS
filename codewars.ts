2c42ee910dc671b1143c4c09cc1cd492c38dccc2
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-04-26 10:25:57 +1000

    codewars: validate pin

--- codewars.ts
+++ codewars.ts
@@ -1,9 +1,13 @@
-function findShort (s:string): number {
-   return Math.min(
-    ...s
-    .split(" ")
-    .map(word => word.length)
-    )
+function validatePin(num:string):boolean{
+    let count = num.length
+    let regex = /\D/g
+    let found =  num.match(regex)
+    return !found && (count === 4 || count ===6)? true : false
 }
 
-console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
+console.log(validatePin("1234"))
+console.log(validatePin("123a"))
+console.log(validatePin("4567"))
+console.log(validatePin("123"))
+console.log(validatePin("12345"))
+console.log(validatePin("1"))
