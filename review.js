89e89b07688640fed0472445a92abf85ee1cf97d
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-05-02 09:45:24 +1000

    review -  regex: or |

--- review.js
+++ review.js
@@ -1,15 +1,4 @@
-// let string = 'titianic'
-// let regex = /t[a-z]*i/
-// let result = string.match(regex)
-// console.log(result)
-
-
-let text = '<h1>Winter is coming </h1>';
-let myRegex = /<.*>/
-let result2 = text.match(myRegex)
-console.log(result2)
-
-// let text3 = 'Hello, world'
-// let regex3 = /hello/i
-// let result3 = text3.match(regex3)
-// console.log(result3)
+let string = "Extract the word 'coding' from this string"
+let regex = /word|coding/g
+let result = string.match(regex)
+console.log(result)
