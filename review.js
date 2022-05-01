969e8ad4c7b055c591d3efe076ec3329d8f8ca71
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-05-01 12:04:16 +1000

    review: regex - lazy and greedy matching

--- review.js
+++ review.js
@@ -1,4 +1,15 @@
-let string = "Mississipis"
-let regex = /s+/g
-let result = string.match(regex)
-console.log(result)
+// let string = 'titianic'
+// let regex = /t[a-z]*i/
+// let result = string.match(regex)
+// console.log(result)
+
+
+let text = '<h1>Winter is coming </h1>';
+let myRegex = /<.*>/
+let result2 = text.match(myRegex)
+console.log(result2)
+
+// let text3 = 'Hello, world'
+// let regex3 = /hello/i
+// let result3 = text3.match(regex3)
+// console.log(result3)
