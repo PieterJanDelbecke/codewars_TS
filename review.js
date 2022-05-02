2ae435d028a21347c59275e30ed11930bec11540
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-05-02 13:51:03 +1000

    regex: positive and negative lookahead

--- review.js
+++ review.js
@@ -1,4 +1,5 @@
-let string = "Timmmmber"
-let regex = /Tim{3}ber/
-let result = regex.test(string)
+let sampleWord = "1astr23ona3ut"
+let regex = /(?=\w{5})(?=\D*\d{2})/
+// let regex = /(?=\w{5})/
+let result =  regex.test(sampleWord)
 console.log(result)
