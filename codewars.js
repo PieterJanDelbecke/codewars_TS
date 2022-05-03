6e46535c4e431dd4c03546d88d41fd54fefc59f4
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-05-03 14:52:00 +1000

    codewars: sum positives and negatives

--- codewars.js
+++ codewars.js
@@ -1,15 +1,16 @@
 "use strict";
-function accum(s) {
-    let arr = [];
-    s.toLowerCase().split("").forEach((letter, index) => {
-        arr.push(letter.toUpperCase());
-        for (let i = 0; i < index; i++) {
-            arr.push(letter);
-        }
-        if (index < s.length - 1) {
-            arr.push("-");
-        }
-    });
-    return arr.join("");
+function countPositives(input) {
+    let pos = 0;
+    let neg = 0;
+    if (input === null || input.length === 0) {
+        return [];
+    }
+    else {
+        input.forEach((e) => e > 0 ? pos++ : neg += e);
+        return [pos, neg];
+    }
 }
-console.log(accum("ZpglnRxqenU"));
+console.log(countPositives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
+console.log(countPositives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]));
+console.log(countPositives([]));
+console.log(countPositives(null));
--- codewars.ts
+++ codewars.ts
@@ -1,15 +1,23 @@
-function accum(s: string){
-    let arr:string[] = []
-    s.toLowerCase().split("").forEach((letter, index)=>{
-        arr.push(letter.toUpperCase())
-        for (let i = 0 ; i < index; i++){
-            arr.push(letter)
-        }
-        if (index < s.length-1){
-            arr.push("-")
-        }
-    })
-    return arr.join("")
+function countPositives(input: any) {
+  let pos: number = 0
+  let neg: number = 0;
+  if (input === null || input.length === 0){
+      return []
+  } else {
+      input.forEach((e:number) => e > 0 ? pos++ : neg += e);
+      return [pos, neg] 
+}
 }
 
-console.log(accum("ZpglnRxqenU"))
+console.log(
+  countPositives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
+);
+console.log(
+  countPositives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14])
+);
+console.log(
+    countPositives([])
+)
+console.log(
+    countPositives(null)
+)
