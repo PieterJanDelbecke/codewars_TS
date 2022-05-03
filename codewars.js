7f34fad57b312f35d38e4c87007ce4c4cc20c547
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-05-03 16:44:24 +1000

    codewars: eightBitNumbers

--- codewars.js
+++ codewars.js
@@ -1,11 +1,16 @@
 "use strict";
-function validatePin(pin) {
-    let regex = /^\d{4}$|^\d{6}$/;
-    return regex.test(pin);
+function eightNumberString(str) {
+    //   let regex = /^(\d | [1-9]\d | 1\d\d | 2[0-4]\d | 25[0-5])$/;
+    let regex = /(^[1-9]?[0-9]{1}$)|(^1[0-9][0-9]$)|(^2[0-4][0-9]$)|(^25[0-5]$)/;
+    return regex.test(str);
 }
-console.log(validatePin("1"));
-console.log(validatePin("12"));
-console.log(validatePin("123"));
-console.log(validatePin("1234"));
-console.log(validatePin("12345"));
-console.log(validatePin("123456"));
+console.log(eightNumberString(""));
+console.log(eightNumberString("0"));
+console.log(eightNumberString("00"));
+console.log(eightNumberString("55"));
+console.log(eightNumberString("042"));
+console.log(eightNumberString("123"));
+console.log(eightNumberString("240"));
+console.log(eightNumberString("255"));
+console.log(eightNumberString("256"));
+console.log(eightNumberString("999"));
