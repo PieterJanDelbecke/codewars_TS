05e890678f010f1c518b3a9a35fee063b8c1076a
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-05-03 15:32:10 +1000

    codewars: validate PIN

--- codewars.js
+++ codewars.js
@@ -1,16 +1,11 @@
 "use strict";
-function countPositives(input) {
-    let pos = 0;
-    let neg = 0;
-    if (input === null || input.length === 0) {
-        return [];
-    }
-    else {
-        input.forEach((e) => e > 0 ? pos++ : neg += e);
-        return [pos, neg];
-    }
+function validatePin(pin) {
+    let regex = /^\d{4}$|^\d{6}$/;
+    return regex.test(pin);
 }
-console.log(countPositives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
-console.log(countPositives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]));
-console.log(countPositives([]));
-console.log(countPositives(null));
+console.log(validatePin("1"));
+console.log(validatePin("12"));
+console.log(validatePin("123"));
+console.log(validatePin("1234"));
+console.log(validatePin("12345"));
+console.log(validatePin("123456"));
--- codewars.ts
+++ codewars.ts
@@ -1,23 +1,11 @@
-function countPositives(input: any) {
-  let pos: number = 0
-  let neg: number = 0;
-  if (input === null || input.length === 0){
-      return []
-  } else {
-      input.forEach((e:number) => e > 0 ? pos++ : neg += e);
-      return [pos, neg] 
-}
+function validatePin(pin: string){
+    let regex = /^\d{4}$|^\d{6}$/
+    return regex.test(pin)
 }
 
-console.log(
-  countPositives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
-);
-console.log(
-  countPositives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14])
-);
-console.log(
-    countPositives([])
-)
-console.log(
-    countPositives(null)
-)
+console.log(validatePin("1"))
+console.log(validatePin("12"))
+console.log(validatePin("123"))
+console.log(validatePin("1234"))
+console.log(validatePin("12345"))
+console.log(validatePin("123456"))
