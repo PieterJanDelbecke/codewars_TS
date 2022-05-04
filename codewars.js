f9ae116ff9a363405106d199ffa149febda44ab4
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-05-04 10:00:14 +1000

    codewars: anchorize

--- codewars.js
+++ codewars.js
@@ -1,16 +1,8 @@
 "use strict";
-function eightNumberString(str) {
-    //   let regex = /^(\d | [1-9]\d | 1\d\d | 2[0-4]\d | 25[0-5])$/;
-    let regex = /(^[1-9]?[0-9]{1}$)|(^1[0-9][0-9]$)|(^2[0-4][0-9]$)|(^25[0-5]$)/;
-    return regex.test(str);
+function anchorize(text) {
+    const regex = /((http|https|ftp|ftps|file|smb)\S+)/ig;
+    let newStr = '<a href="$1">$1</a>';
+    return text.replace(regex, newStr);
 }
-console.log(eightNumberString(""));
-console.log(eightNumberString("0"));
-console.log(eightNumberString("00"));
-console.log(eightNumberString("55"));
-console.log(eightNumberString("042"));
-console.log(eightNumberString("123"));
-console.log(eightNumberString("240"));
-console.log(eightNumberString("255"));
-console.log(eightNumberString("256"));
-console.log(eightNumberString("999"));
+console.log(anchorize("hello FTP://world.com !"));
+// 'hello <a href="FTP://world.com">FTP://world.com</a> !'
--- review.js
+++
@@ -1,2 +0,0 @@
-let result = "code camp".replace(/(\w+)\s(\w+)/, "$2 $1")
-console.log(result)
