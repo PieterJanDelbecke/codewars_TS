15adb37920cf4ebc94061b018c39337f8dc2d57c
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-05-12 10:40:54 +1000

    review: this keyword

--- app.ts
+++ app.ts
@@ -4,8 +4,12 @@ class Department {
   constructor (n:string){
     this.name = n;
   }
+
+  describe(this:Department) {
+    console.log("Department: " + this.name)
+  }
 }
 
 const accounting = new Department('Accounting')
 
-console.log(accounting)
+accounting.describe()
