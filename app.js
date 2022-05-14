f49ba3ada7a85d40af08374038242a5f4ad84bf6
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-05-15 09:05:24 +1000

    ts review: overriding properties

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ class Department {
         console.log(`department: ${this.name}, ID: ${this.id}`);
     }
     addEmployee(employee) {
-        this.employees.push(employee);
+        // this.employees.push(employee)
+        this.employees = [...this.employees, employee];
     }
     outputInfo() {
         console.log(`number of employees: ${this.employees.length}`);
@@ -24,8 +25,35 @@ class ITDepartment extends Department {
         this.admins = admins;
     }
 }
-const IT = new ITDepartment("D6", ['Pieter', 'Max']);
-IT.addEmployee('Alessandra');
-IT.describe();
-IT.outputInfo();
-console.log(IT.admins);
+class AccountingDepartment extends Department {
+    constructor(id, reports) {
+        super(id, 'Accounting');
+        this.reports = reports;
+    }
+    addReport(text) {
+        this.reports.push(text);
+    }
+    printReports() {
+        console.log(this.reports);
+    }
+    addEmployee(name) {
+        if (name === "Max") {
+            return;
+        }
+        this.employees.push(name);
+    }
+}
+// const IT = new ITDepartment("D6", ['Pieter', 'Max'])
+// IT.addEmployee('Alessandra')
+// IT.addEmployee('John')
+// IT.addEmployee('Marc')
+// IT.describe()
+// IT.outputInfo()
+// console.log(IT.admins)
+const accounting = new AccountingDepartment('d2', []);
+accounting.addReport("something went wrong...");
+accounting.printReports();
+accounting.addEmployee("Kurt");
+accounting.addEmployee("Max");
+accounting.addEmployee("Josh");
+accounting.outputInfo();
--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 class Department {
   private id: string
   private name: string
-  private employees: string[] = []
+  protected employees: string[] = []
 
   constructor ( id: string, name: string){
     this.id = id
@@ -13,7 +13,8 @@ class Department {
   }
 
   addEmployee(employee:string){
-    this.employees.push(employee)
+    // this.employees.push(employee)
+    this.employees = [...this.employees, employee]
   }
 
   outputInfo(){
@@ -35,8 +36,36 @@ class ITDepartment extends Department {
 
 }
 
-const IT = new ITDepartment("D6", ['Pieter', 'Max'])
-IT.addEmployee('Alessandra')
-IT.describe()
-IT.outputInfo()
-console.log(IT.admins)
+class AccountingDepartment extends Department {
+  constructor (id: string, private reports: string[]){
+    super(id, 'Accounting')
+  }
+  addReport(text:string){
+    this.reports.push(text)
+  }
+  printReports(){
+    console.log(this.reports)
+  }
+  addEmployee (name:string){
+    if (name==="Max"){
+      return
+    }
+    this.employees.push(name)
+  }
+}
+
+// const IT = new ITDepartment("D6", ['Pieter', 'Max'])
+// IT.addEmployee('Alessandra')
+// IT.addEmployee('John')
+// IT.addEmployee('Marc')
+// IT.describe()
+// IT.outputInfo()
+// console.log(IT.admins)
+
+const accounting = new AccountingDepartment ('d2',[])
+accounting.addReport("something went wrong...")
+accounting.printReports()
+accounting.addEmployee("Kurt")
+accounting.addEmployee("Max")
+accounting.addEmployee("Josh")
+accounting.outputInfo()
