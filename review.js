a4397352768d2fabad90521d8ce58142444acac0
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-05-19 09:00:43 +1000

    review Objects

--- review.js
+++ review.js
@@ -1,11 +1,22 @@
-const data = {
-    en: "english",
-    nl: "dutch",
-    it: "italian"
-}
+const FW = [
+    {film: 'film1', price: 28},
+    {film: 'film2', price: 25},
+    {film: 'film3', price: 30},
+]
 
-let newArray = []
+const CW = [
+    {film: 'film1', price: 30},
+    {film: 'film2', price: 21},
+    {film: 'film3', price: 35},
+]
 
-for (const [key, value] of Object.entries(data)){
-    newArray.push({id: key, lang: value})
-}
+
+let arr = []
+
+FW.forEach((movie, index) => {
+    arr.push({film: movie.film, priceFW: movie.price, priceCW: CW[index].price})
+})
+
+console.log(arr)
+
+
