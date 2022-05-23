bc6c544999a7c6ceb8623d922ca1e812028c299b
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-05-24 08:38:23 +1000

    css review: ::before ::after

--- review.js
+++ review.js
@@ -1,31 +1,3 @@
-const FW = [
-    {title: "film1", price: 40},
-    {title: "film2", price: 20},
-    {title: "film3", price: 15},
-]
-
-const CW = [
-    {title: "film3", price: 18},
-    {title: "film1", price: 35},
-    {title: "film2", price: 25},
-]
-
-films = []
-
-for (const [key, value] of Object.entries(FW)){
-    films.push({
-        title: value.title,
-        priceFW: value.price,
-        priceCW: CW[key].price
-    })
-}
-
-// FW.forEach((film) => {
-//     films.push({
-//         title: film.title,
-//         priceFW: film.price,
-//         priceCW: CW[film.title].price
-//     })
-// })
-
-console.log(films)
+let string = " Hello world "
+console.log(string)
+console.log(string.trim())
