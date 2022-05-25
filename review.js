41bb68af4233a54b0c3a950b03d9ec132329711f
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-05-25 10:36:23 +1000

    select object out of an array

--- review.js
+++ review.js
@@ -1,3 +1,8 @@
-let string = " Hello world "
-console.log(string)
-console.log(string.trim())
+let hotels = [
+    {id: 1, hotel: "one"},
+    {id: 2, hotel: "two"},
+    {id: 3, hotel: "three"}
+]
+
+let chosenHotel = hotels.filter(hotel => hotel.id === 2)
+console.log(chosenHotel[0])
