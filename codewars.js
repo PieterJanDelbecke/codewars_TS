a9b2916992c14720fe98df145e05e4b8d8d00b8a
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-08-26 08:25:12 +1000

    JS review: Array - fill

--- codewars.js
+++ codewars.js
@@ -1,4 +1,7 @@
-let string = " Hello World "
-console.log(`*${string}*`)
-string = string.replaceAll(" ", "_")
-console.log(`*${string}*`)
+// let string = " Hello World "
+// console.log(`*${string}*`)
+// string = string.replaceAll(" ", "_")
+// console.log(`*${string}*`)
+
+const arr = Array(4).fill(false)
+console.log(arr)
