70150c30ae5790eb20d3510d697f6ba2c0d20bcc
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-08-26 08:37:35 +1000

    js review - splice

--- codewars.js
+++ codewars.js
@@ -3,5 +3,12 @@
 // string = string.replaceAll(" ", "_")
 // console.log(`*${string}*`)
 
-const arr = Array(4).fill(false)
-console.log(arr)
+// const arr = Array(4).fill(false)
+// console.log(arr)
+
+// const arr = [1,10,100,2,20,200,3,30,300]
+
+let numbers = ["one", "two", "three", "four", "five"]
+numbers.splice(1,0,"XXX")
+console.log(numbers)
+
