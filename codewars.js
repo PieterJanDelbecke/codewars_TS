8392c98ee99cdc82561c42a105e12917ae0d472f
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-08-26 08:53:48 +1000

    js review sort

--- codewars.js
+++ codewars.js
@@ -6,11 +6,17 @@
 // const arr = Array(4).fill(false)
 // console.log(arr)
 
-// let numbers = [1,10,100,2,20,200,3,30,300]
-let numbers = [1, "one", 2, "two", 3, "three"]
-console.log(numbers)
-numbers = numbers.filter( number => typeof number !== 'number')
-console.log(numbers)
+let numbers = [1,10,100,2,20,200,3,30,300]
+let numbersUp = numbers.sort((a, b) => b - a)
+// numbersUp = numbers.sort()
+console.log(numbersUp)
+let numbersDown = numbers.sort((a, b) => a - b)
+console.log(numbersDown)
+
+// let numbers = [1, "one", 2, "two", 3, "three"]
+// console.log(numbers)
+// numbers = numbers.filter( number => typeof number !== 'number')
+// console.log(numbers)
 
 // let numbers = ["one", "two", "three", "four", "five"]
 // numbers.splice(1,0,"XXX")
