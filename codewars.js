3bbf3fa4060f9183f5f79992387ec9dc82568484
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-09-05 07:41:42 +1000

    review JS

--- codewars.js
+++ codewars.js
@@ -1,24 +1,7 @@
-// const numbers = [1,2,3,4,5,6,7,8,9]
-const numbers = {
-    one: 1,
-    two: 2,
-    three: 3
-}
+const myArray1 = [0,1,2,3,4,5]
 
-// const result = numbers.filter(number => number%2 === 0)
-// const result = numbers.reduce((a,b) => a + b)
-// const result = numbers.sort((a,b) => b-a)
-// for (let number of numbers){
-//     console.log("number", number)
-// }
+const myArray2 = [...myArray1,6,7,8,9]
 
-// console.log(result)
+const [a, b, c,...x] = myArray2
 
-// numbers.forEach((number, index)=> {
-//     console.log(`${index+1}: ${number}`)
-// })
-
-const values = Object.values(numbers)
-console.log(values)
-const keys = Object.keys(numbers)
-console.log(keys)
+console.log({a,b,c,x})
