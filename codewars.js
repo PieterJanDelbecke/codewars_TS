b90d47e03d7d3d79fc84c0c4a80072b2e9308a2e
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-09-05 07:57:25 +1000

    review JS - array methods

--- codewars.js
+++ codewars.js
@@ -1,7 +1,13 @@
-const myArray1 = [0,1,2,3,4,5]
+const myArray1 = [0, 1, 2, 3, 4, 5];
+const myArray2 = [...myArray1, 6, 7, 8, 9];
 
-const myArray2 = [...myArray1,6,7,8,9]
+// myArray2.sort((a, b) => b - a);
+// console.log(mySort);
+// console.log(myArray2)
 
-const [a, b, c,...x] = myArray2
+// const myFilter = myArray2.filter((item)=> item%2 === 0)
+// console.log(myArray2)
+// console.log(myFilter)
 
-console.log({a,b,c,x})
+const myFind = myArray2.find((item) => item > 6)
+console.log(myFind)
