878b174695ded50a0d7a6565236ba9b6fd5cef56
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-09-05 08:58:37 +1000

    review JS - methods

--- codewars.js
+++ codewars.js
@@ -1,13 +1,4 @@
-const myArray1 = [0, 1, 2, 3, 4, 5];
-const myArray2 = [...myArray1, 6, 7, 8, 9];
+let myArray = [0,1,2,3,4,5,6,7,8,9]
 
-// myArray2.sort((a, b) => b - a);
-// console.log(mySort);
-// console.log(myArray2)
-
-// const myFilter = myArray2.filter((item)=> item%2 === 0)
-// console.log(myArray2)
-// console.log(myFilter)
-
-const myFind = myArray2.find((item) => item > 6)
-console.log(myFind)
+let result = myArray.splice(1,1)
+console.log({myArray, result})
