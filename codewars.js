017d99ea12558b273240d14d0439c8a11250ce7f
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-09-07 08:01:09 +1000

    JS review

--- codewars.js
+++ codewars.js
@@ -1,17 +1,18 @@
-function findShort (s){
-    let count = 100
-    let arr = s.split(" ").forEach(item => {
-        if (item.length < count){
-            count = item.length
-        }
-    })
-    return count
-}
+// function findShort (s){
+//     let count = 100
+//     let arr = s.split(" ").forEach(item => {
+//         if (item.length < count){
+//             count = item.length
+//         }
+//     })
+//     return count
+// }
 
-console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
-console.log(findShort("turns out random test cases are easier then writin out basic ones"))
-console.log(findShort("Let's travel abroud shall we"))
+// console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
+// console.log(findShort("turns out random test cases are easier then writin out basic ones"))
+// console.log(findShort("Let's travel abroud shall we"))
 
-// myString = "five"
-// myArray = [0,1,2,3]
-// console.log(myString.length)
+myString = "five"
+myArray = [0,1,2,3,4]
+console.log(myString.length)
+console.log(myArray.length)
