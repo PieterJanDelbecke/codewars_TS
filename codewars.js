3b396ca9704800f88734a5505b7f676bfdbf65ab
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-09-07 08:03:22 +1000

    codewars findShort refactored

--- codewars.js
+++ codewars.js
@@ -1,18 +1,16 @@
-// function findShort (s){
-//     let count = 100
-//     let arr = s.split(" ").forEach(item => {
-//         if (item.length < count){
-//             count = item.length
-//         }
-//     })
-//     return count
-// }
+function findShort (s){
+    let count = 100
+    s.split(" ").forEach(item => {
+        if (item.length < count) count = item.length
+    })
+    return count
+}
 
-// console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
-// console.log(findShort("turns out random test cases are easier then writin out basic ones"))
-// console.log(findShort("Let's travel abroud shall we"))
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
+console.log(findShort("turns out random test cases are easier then writin out basic ones"))
+console.log(findShort("Let's travel abroud shall we"))
 
-myString = "five"
-myArray = [0,1,2,3,4]
-console.log(myString.length)
-console.log(myArray.length)
+// myString = "five"
+// myArray = [0,1,2,3,4]
+// console.log(myString.length)
+// console.log(myArray.length)
