727e56a2c828dd7f201cd92b23ef3358b11cc931
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-09-07 08:13:45 +1000

    codewars Math.min

--- codewars.js
+++ codewars.js
@@ -1,16 +1,13 @@
 function findShort (s){
-    let count = 100
-    s.split(" ").forEach(item => {
-        if (item.length < count) count = item.length
-    })
-    return count
+  let arr = s.split(' ').map(i => i.length)
+  return Math.min(...arr)
 }
 
 console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
 console.log(findShort("turns out random test cases are easier then writin out basic ones"))
 console.log(findShort("Let's travel abroud shall we"))
 
-// myString = "five"
-// myArray = [0,1,2,3,4]
-// console.log(myString.length)
-// console.log(myArray.length)
+// const arr = [0,1,2,3,4,5]
+
+// console.log(Math.min(...arr))
+// console.log(Math.max(...arr))
