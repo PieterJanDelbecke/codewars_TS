6b3493835fd169aa20192f7fd3443c409b151e1e
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-09-30 08:21:54 +1000

    JS review: Set & Map

--- codewars.js
+++ codewars.js
@@ -1,18 +1,25 @@
-c1 = {
-    x: 10,
-    y: 20
-}
-c2 ={
-    x: 33,
-    y: 66
+const initialColumns = [
+    ["name", { value: "Name", type: "text" }],
+    ["status", { value: "Status", type: "text" }],
+    ["priority", { value: "Priority", type: "text" }],
+    ["email", { value: "Email", type: "text" }],
+    ["phone", { value: "Phone", type: "text" }],
+    ["source", { value: "Source", type: "text" }],
+    ["from", { value: "From", type: "text" }],
+];
+
+const myMap = new Map(
+    initialColumns.map((column, index) => {
+        column[1].order = index;
+        return column;
+    })
+)
+
+const set = new Set()
+const numbers = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9]
+for (let number of numbers ){
+    set.add(number)
 }
 
-function printCoordinates(){
-    console.log(this.x, this.y)
-}
-
-c1_func = printCoordinates.bind(c1)
-c2_func = printCoordinates.bind(c2)
-
-c1_func()
-c2_func()
+console.log(...myMap)
+console.log([...set])
