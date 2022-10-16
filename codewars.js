bcc94569fae0df667096a14cfecfb8c2f2aa3160
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-10-17 09:03:24 +1100

    JS review: Maps within Map

--- codewars.js
+++ codewars.js
@@ -1,11 +1,8 @@
-const myObj = {
-    one: 1,
-    two: 2,
-    three: 3
-}
+const myMap = new Map()
 
-const string = JSON.stringify(myObj)
+myMap.set("uuid", new Map())
 
-const back = JSON.parse(string)
+myMap.get("uuid").set('myObj', {one: "one", two: "two", three: "tree"})
+myMap.get("uuid").set('myArr', [1,2,3,4,5,6,7,8,9])
 
-console.log(back)
+console.log(myMap.get("uuid").get("myArr"))
