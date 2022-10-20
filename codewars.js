ab0002bd6b2dcb4b55e8530f31278e5370f94b41
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-10-20 12:53:59 +1100

    js review: formData

--- codewars.js
+++ codewars.js
@@ -1,8 +1,3 @@
-const myMap = new Map()
+const formData = new FormData()
 
-myMap.set("uuid", new Map())
-
-myMap.get("uuid").set('myObj', {one: "one", two: "two", three: "tree"})
-myMap.get("uuid").set('myArr', [1,2,3,4,5,6,7,8,9])
-
-console.log(myMap.get("uuid").get("myArr"))
+console.log(formData)
