2a826af59540cf2340477bbe4a554352e0299694
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-05-30 09:31:33 +1000

    vowel count: refactor

--- codewars.js
+++ codewars.js
@@ -1,6 +1,7 @@
-function getCount(string) {
-  const vowels = string.match(/[aeiou]/gi);
+function getCount(str) {
+  const vowels = str.match(/[aeiou]/gi);
   return vowels ? vowels.length : 0;
 }
 
 console.log(getCount("abracadabra"));
+console.log(getCount("qwrty"));
