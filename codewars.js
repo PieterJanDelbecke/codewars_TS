62d6d676e4cb604a37ea4a3134b55379d648947b
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-08-22 09:27:12 +1000

    vowelcount refactor pt2

--- codewars.js
+++ codewars.js
@@ -1,8 +1,12 @@
+// function getCount(str) {
+//   const regex = /[aouie]/gi;
+//   const found = str.match(regex);
+//   return found ? found.join("").length : 0;
+// }
+
 function getCount(str) {
-  const regex = /[aouie]/gi;
-  const found = str.match(regex);
-  return found ? found.join("").length : 0;
+  return (str.match(/[aeiou]/gi) || []).length;
 }
 
-// console.log(getCount("Hello"));
+console.log(getCount("Hello"));
 console.log(getCount("my pyx"));
