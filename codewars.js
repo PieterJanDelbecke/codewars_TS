a7c7c22d402c64ae45eca7e995768e0c23398014
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-08-24 10:48:19 +1000

    codewars: complemantary DNA

--- codewars.js
+++ codewars.js
@@ -1,5 +1,25 @@
-function findShort(s) {
-  return Math.min(...s.split(" ").map((word) => word.length));
+function DNAStrand(dna) {
+  return dna
+    .split("")
+    .map((i) => {
+      switch (i) {
+        case "A":
+          return "T";
+          break;
+        case "T":
+          return "A";
+          break;
+        case "C":
+          return "G";
+          break;
+        case "G":
+          return "C";
+          break;
+      }
+    })
+    .join("");
 }
 
-console.log(findShort("bitcoin take over the world maybe who knows perhaps"));
+console.log(DNAStrand("AAAA"));
+console.log(DNAStrand("ATTGC"));
+console.log(DNAStrand("GTAT"));
