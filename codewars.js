e80305b0fbd099cf7a42efc30cccecf10d1edcff
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-08-24 11:47:46 +1000

    Sum of Numbers

--- codewars.js
+++ codewars.js
@@ -1,6 +1,15 @@
-function sumTwoSmallestNumbes(numbers) {
-  numbers = numbers.sort((a, b) => a - b);
-  return numbers[0] + numbers[1];
+function getSum(a, b) {
+  [a, b] = [a, b].sort((c, d) => c - d);
+  let sum = 0;
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+  return sum;
 }
 
-console.log(sumTwoSmallestNumbes([5, 8, 12, 19, 22]));
+console.log(getSum(1, 0));
+console.log(getSum(1, 2));
+console.log(getSum(0, 1));
+console.log(getSum(1, 1));
+console.log(getSum(-1, 0));
+console.log(getSum(-1, 2));
