210bebd6e3bf325bea4a5584d7b7958823966650
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-08-25 15:57:48 +1000

    codewars: Regex validate PIN code

--- codewars.js
+++ codewars.js
@@ -1,9 +1,15 @@
-function printerError(s) {
-  const regex = /[n-z]/gi;
-  const found = s.match(regex);
-  return `${found.length}/${s.length}`;
+function validatePIN(pin) {
+  const regex = /[^0-9]/;
+  if (regex.test(pin)) {
+    return false;
+  } else {
+    return pin.length === 4 || pin.length === 6 ? true : false;
+  }
 }
 
-console.log(
-  printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz")
-);
+// console.log(validatePIN("1234"));
+// console.log(validatePIN("12345"));
+// console.log(validatePIN("a234"));
+console.log(validatePIN("-12345"));
+console.log(validatePIN("12.0"));
+console.log(validatePIN("32?5"));
