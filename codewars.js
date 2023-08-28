818e2559c819c90fd4d2df715e4cb49e111ec579
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-08-28 10:52:03 +1000

    codewars: growth of a population

--- codewars.js
+++ codewars.js
@@ -1,15 +1,13 @@
-function validatePIN(pin) {
-  const regex = /[^0-9]/;
-  if (regex.test(pin)) {
-    return false;
-  } else {
-    return pin.length === 4 || pin.length === 6 ? true : false;
+function nbYear(p0, percent, aug, p) {
+  let total = p0;
+  let count = 0;
+  while (total <= p) {
+    total = Math.floor(total + total * percent * 0.01 + aug);
+    count++;
   }
+  return count;
 }
 
-// console.log(validatePIN("1234"));
-// console.log(validatePIN("12345"));
-// console.log(validatePIN("a234"));
-console.log(validatePIN("-12345"));
-console.log(validatePIN("12.0"));
-console.log(validatePIN("32?5"));
+console.log(nbYear(1500, 5, 100, 5000));
+console.log(nbYear(1500000, 2.5, 10000, 2000000));
+console.log(nbYear(1500000, 0.25, 1000, 2000000));
