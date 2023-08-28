e4cc215e4804f53a258a8ac796ed650e86a3ec25
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-08-29 09:29:38 +1000

    codewars: odd or even

--- codewars.js
+++ codewars.js
@@ -1,25 +1,12 @@
-function busStops(numbers) {
-  let total = 0;
-  numbers.forEach(([on, off]) => {
-    total += on - off;
-  });
-  return total;
+function oddOrEven(array) {
+  if (array.length === 0) return "even";
+  const myArr = array.reduce((a, b) => a + b);
+  return myArr % 2 === 0 ? "even" : "odd";
 }
 
-console.log(
-  busStops([
-    [10, 0],
-    [3, 5],
-    [5, 8],
-  ])
-);
-console.log(
-  busStops([
-    [3, 0],
-    [9, 1],
-    [4, 10],
-    [12, 2],
-    [6, 1],
-    [7, 10],
-  ])
-);
+console.log(oddOrEven([0, 1, 5]));
+console.log(oddOrEven([0, 1, 3]));
+console.log(oddOrEven([1023, 1, 2]));
+console.log(oddOrEven([]));
+console.log(oddOrEven([0]));
+console.log(oddOrEven([1]));
