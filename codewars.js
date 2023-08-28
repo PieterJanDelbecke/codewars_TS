79b49a86d450fea672882e09bd047797763a001b
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-08-28 11:14:11 +1000

    Binary Addition

--- codewars.js
+++ codewars.js
@@ -1,21 +1,5 @@
-function nbYear(p0, percent, aug, p) {
-  count = 0;
-  for (total = p0; total < p; count++) {
-    total = Math.floor(total + total * percent * 0.01 + aug);
-  }
-  return count;
+function addBinary(a, b) {
+  return (a + b).toString(2);
 }
 
-// function nbYear(p0, percent, aug, p) {
-//   let total = p0;
-//   let count = 0;
-//   while (total <= p) {
-//     total = Math.floor(total + total * percent * 0.01 + aug);
-//     count++;
-//   }
-//   return count;
-// }
-
-console.log(nbYear(1500, 5, 100, 5000));
-console.log(nbYear(1500000, 2.5, 10000, 2000000));
-console.log(nbYear(1500000, 0.25, 1000, 2000000));
+console.log(addBinary(1, 2));
