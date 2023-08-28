a8de0996ec67cf8d1e233317177df369d14bda24
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-08-29 09:18:02 +1000

    codewars: Number of people in the bus - refactor

--- codewars.js
+++ codewars.js
@@ -1,7 +1,6 @@
 function busStops(numbers) {
   let total = 0;
-  numbers.forEach((item) => {
-    const [on, off] = item;
+  numbers.forEach(([on, off]) => {
     total += on - off;
   });
   return total;
@@ -12,5 +11,15 @@ console.log(
     [10, 0],
     [3, 5],
     [5, 8],
+  ])
+);
+console.log(
+  busStops([
+    [3, 0],
+    [9, 1],
+    [4, 10],
+    [12, 2],
+    [6, 1],
+    [7, 10],
   ])
 );
