d1e75448c1da6cd7762decc967175c316448d2dd
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-08-30 09:52:00 +1000

    remove the minimum

--- codewars.js
+++ codewars.js
@@ -1,12 +1,12 @@
-function divisors(integer) {
-  const divisorsArr = [];
-  for (let i = 2; i < integer; i++) {
-    if (integer % i === 0) {
-      divisorsArr.push(i);
-    }
-  }
-  return divisorsArr.length ? `${integer} is prime` : divisorsArr;
+function removeSmallest(numbers) {
+  const myNumbers = [...numbers];
+  const smallest = Math.min(...myNumbers);
+  const index = myNumbers.findIndex((num) => num === smallest);
+  myNumbers.splice(index, 1);
+  return myNumbers;
 }
 
-console.log(divisors(15));
-console.log(divisors(13));
+console.log(removeSmallest([1, 2, 3, 4, 5]));
+console.log(removeSmallest([5, 3, 2, 1, 4]));
+console.log(removeSmallest([2, 2, 1, 2, 1]));
+console.log(removeSmallest([]));
