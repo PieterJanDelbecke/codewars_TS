90de6f40b4746fe6aba7e5ba32b8e97d82c41a60
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-08-30 10:01:31 +1000

    codewars: testing 1-2-3

--- codewars.js
+++ codewars.js
@@ -1,12 +1,10 @@
-function removeSmallest(numbers) {
-  const myNumbers = [...numbers];
-  const smallest = Math.min(...myNumbers);
-  const index = myNumbers.findIndex((num) => num === smallest);
-  myNumbers.splice(index, 1);
-  return myNumbers;
+function testing(array) {
+  const arr = [];
+  array.forEach((num, index) => {
+    arr.push(`${index + 1}: ${num}`);
+  });
+  return arr;
 }
 
-console.log(removeSmallest([1, 2, 3, 4, 5]));
-console.log(removeSmallest([5, 3, 2, 1, 4]));
-console.log(removeSmallest([2, 2, 1, 2, 1]));
-console.log(removeSmallest([]));
+console.log(testing(["a", "b", "c"]));
+console.log(testing([]));
