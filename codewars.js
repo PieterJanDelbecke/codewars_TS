1be5c35e828e3cab830b2bce705f58a04efbe81c
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-08-30 10:03:30 +1000

    codewars: testing 1-2-3 - refactor

--- codewars.js
+++ codewars.js
@@ -1,9 +1,7 @@
 function testing(array) {
-  const arr = [];
-  array.forEach((num, index) => {
-    arr.push(`${index + 1}: ${num}`);
+  return array.map((num, index) => {
+    return `${index + 1}: ${num}`;
   });
-  return arr;
 }
 
 console.log(testing(["a", "b", "c"]));
