7fc7aec8bee3583c0ddcab4a4dac6e12ffacf33f
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-09-07 09:03:47 +1000

    codewars: sum of a sequence - refactor

--- codewars.js
+++ codewars.js
@@ -1,10 +1,11 @@
 function sequenceSum(begin, end, step) {
-  const arr = [];
+  let sum = 0;
   for (let i = begin; i <= end; i += step) {
-    arr.push(i);
+    sum += i;
   }
-  return arr.reduce((a, b) => a + b);
+  return sum;
 }
 
-console.log(sequenceSum(2, 6, 2));
-console.log(sequenceSum(1, 5, 1));
+// console.log(sequenceSum(2, 6, 2));
+// console.log(sequenceSum(1, 5, 1));
+console.log(sequenceSum(0, 1, 5));
