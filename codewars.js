8b99db6ed3ccb6f8233a72ff2380a6830d576b50
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-09-13 11:08:41 +1000

    codewars: sum of Digits / Digital Root

--- codewars.js
+++ codewars.js
@@ -1,22 +1,19 @@
-function likes(names) {
-  switch (names.length) {
-    case 0:
-      return "no one likes this";
-    case 1:
-      return `${names[0]} likes this`;
-    case 2:
-      return `${names[0]} and ${names[1]} like this`;
-    case 3:
-      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-    default:
-      return `${names[0]}, ${names[1]} and ${
-        names.length - 2
-      } others like this`;
+function digitalRoot(n) {
+  function toArr(n) {
+    const arr = n
+      .toString()
+      .split("")
+      .map((num) => +num);
+    return arr;
   }
+
+  while (n.toString().length !== 1) {
+    n = toArr(n).reduce((a, b) => a + b);
+  }
+  return n;
 }
 
-console.log(likes([]));
-console.log(likes(["Peter"]));
-console.log(likes(["Jacob", "Alex"]));
-console.log(likes(["Max", "John", "Mark"]));
-console.log(likes(["Alex", "Jacob", "Mark", "Max"]));
+console.log(digitalRoot(16));
+console.log(digitalRoot(942));
+console.log(digitalRoot(132189));
+console.log(digitalRoot(493193));
