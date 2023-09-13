d331642a7c65826c32fe59da7ccddd4d30edb260
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-09-13 10:03:21 +1000

    codewars: find the odd int

--- codewars.js
+++ codewars.js
@@ -1,11 +1,16 @@
-function solution(number) {
-  if (number < 0) return 0;
-  let count = 0;
-  for (let i = 1; i < number; i++) {
-    if (i % 3 === 0 || i % 5 === 0) count += i;
+function findOdd(A) {
+  for (i = 0; i < A.length; i++) {
+    const letter = A[i];
+    let count = 0;
+    A.forEach((item) => {
+      if (item === letter) count++;
+    });
+    if (count % 2 === 1) return letter;
   }
-  return count;
 }
 
-console.log(solution(10));
-console.log(solution(-10));
+console.log(findOdd([7]));
+console.log(findOdd([0]));
+console.log(findOdd([1, 1, 2]));
+console.log(findOdd([0, 1, 0, 1, 0]));
+console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
