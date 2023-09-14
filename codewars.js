a3fd9bdad5c753326ac1fc03e23f8e59fff08ea1
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-09-14 11:41:07 +1000

    create Phone Number

--- codewars.js
+++ codewars.js
@@ -1,11 +1,9 @@
-function arrayDiff(a, b) {
-  for (i = 0; i < b.length; i++) {
-    a = a.filter((item) => item !== b[i]);
-  }
-  return a;
+function createPhoneNumber(input) {
+  return input
+    .join("")
+    .replace(/\D/g, "")
+    .replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
 }
 
-console.log(arrayDiff([1, 2], [1]));
-console.log(arrayDiff([1, 2, 2], [1]));
-console.log(arrayDiff([1, 2, 2], []));
-console.log(arrayDiff([1, 2, 3], [1, 2]));
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]));
