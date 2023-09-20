5d530612994c77c2b9eea540965cd433f46994d8
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-09-20 11:24:38 +1000

    codewars: findOutlier

--- codewars.js
+++ codewars.js
@@ -1,11 +1,11 @@
-function countBits(n) {
-  return n
-    .toString(2)
-    .split("")
-    .filter((num) => +num === 1).length;
+function findOutlier(integers) {
+  const odd = [];
+  const even = [];
+  integers.forEach((num) => {
+    num % 2 === 0 ? even.push(num) : odd.push(num);
+  });
+  return even.length === 1 ? even[0] : odd[0];
 }
 
-console.log(countBits(1234));
-// console.log(countBits())
-// console.log(countBits())
-// console.log(countBits())
+console.log(findOutlier([0, 1, 2]));
+console.log(findOutlier([1, 2, 3]));
