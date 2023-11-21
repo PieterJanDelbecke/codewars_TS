f3832299979ba6a364f27a89bc8540b297d3bfd5
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-11-22 08:54:08 +1100

    review js classses

--- codewars.js
+++ codewars.js
@@ -1,10 +1,1 @@
-const bulkFunction = (residentId, items) => {
-	return items.map((item) => {
-		return { residentId, item };
-	});
-};
 
-const residentId = "abc-abc";
-const religions = ["hindu", "muslim"];
-
-console.log(bulkFunction(residentId, religions));
