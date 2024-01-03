43b4ffe681c94ef151ebe6cc01281f9b86bc3681
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2024-01-04 10:33:24 +1100

    checking of residentActivityId

--- codewars.js
+++ codewars.js
@@ -1,1 +1,9 @@
+const activityIds = [];
+for (let i = 0; i < 40; i++) {
+	activityIds.push(i);
+}
 
+const residentActivityId = activityIds[Math.floor(Math.random() * 40)];
+
+console.log("activityIds: ", activityIds);
+console.log("residentActivityId: ", residentActivityId);
